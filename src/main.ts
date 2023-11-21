93f99488ddddfde6f236ea6fb2a70478268d78e9
Varun <varun1997.pokhriyal@gmail.com>
2023-11-21 21:42:18 +0530

    CORS RESOLVED

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  
   app.use(cors());
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
